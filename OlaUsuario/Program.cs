7cbe5819996cf81b3b55d9935236d728914307ce
MWLeonel <wmleonel6@gmail.com>
2020-09-12 09:57:27 -0300

    Olá Usuário

---
+++ OlaUsuario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            //<---Inicio(Digite--->//
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nome}!");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+}
